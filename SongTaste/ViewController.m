2dcb13be5f0a1b55de55a768bd18708f2c5eaaa1
dimsky <dimsky@163.com>
2015-04-18 17:08:13 +0800

    first commit

--- SongTaste/ViewController.m
+++ SongTaste/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()
 
@@ -15,8 +16,10 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [super viewDidLoNad];
+    // Do any additional setup after loading the view, typically from a nib
+    AVPlayer
+    
 }
 
 - (void)didReceiveMemoryWarning {
